2c2a60543bd1866da74ac69e0ec1449cf348f85c
Sylvain Corsini <sylvain.corsini@protonmail.com>
2022-01-11 03:57:43 +0100

    feat: change wording

--- src/lib/blocks/Footer.svelte
+++ src/lib/blocks/Footer.svelte
@@ -1,12 +1,12 @@
 <div class="text-center grid grid-cols-1 gap-2 print:absolute print:bottom-0 print:w-full">
   <div>
     <p class="print:text-xs">
-      Créé avec à
+      Créé avec
       <a href="https://kit.svelte.dev/" target="_blank" class="text-blue-400">SvelteKit</a>
       & <a href="https://tailwindcss.com/" target="_blank" class="text-blue-400">TailwindCSS</a>.
     </p>
     <p class="text-xs">
-      Trouvez le code sur GitHub : <a href="https://github.com/scorsi/resume" class="text-blue-400">github.com/scorsi/resume</a>.
+      Le code du CV est accessible sur GitHub : <a href="https://github.com/scorsi/resume" class="text-blue-400">github.com/scorsi/resume</a>.
     </p>
   </div>
 
