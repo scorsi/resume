d7d8eb25f77d0381da15673e3d96f25eb937982d
Sylvain Corsini <sylvain.corsini@protonmail.com>
2022-01-11 03:10:24 +0100

    feat: add svelte/adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
