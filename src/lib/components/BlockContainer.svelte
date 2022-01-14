0b4afd3441f1d12273a2816f2103305acd84a9ae
Sylvain Corsini <sylvain.corsini@protonmail.com>
2022-01-15 00:23:44 +0100

    feat: center elem in block container

--- src/lib/components/BlockContainer.svelte
+++ src/lib/components/BlockContainer.svelte
@@ -1,7 +1,11 @@
-<section class="mx-6 sm:mx-0 print:mx-0 bg-white rounded-xl md:rounded-2xl p-8 shadow-inner print:p-4 print:rounded-lg print:border print:shadow-none">
+<section class="flex flex-col mx-6 sm:mx-0 print:mx-0 bg-white rounded-xl md:rounded-2xl p-8 shadow-inner print:p-4 print:rounded-lg print:border print:shadow-none">
   <div class="text-xl sm:text-2xl md:text-4xl print:text-xl" >
     <slot name="title" />
   </div>
   <hr class="mb-4 sm:mb-6 md:mb-8 mt-2 print:mb-4" />
-  <slot name="content" />
+  <div class="w-full grow flex">
+    <div class="w-full self-center">
+      <slot name="content" />
+    </div>
+  </div>
 </section>
