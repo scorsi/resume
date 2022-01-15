d0fdacd56f2404c16371ca35a1340be629425a8f
scorsi <sylvain.corsini@protonmail.com>
2022-01-15 04:13:55 +0000

    Deploying to gh-pages from @ scorsi/resume@473177856e0879cb0c0d4a2d077ca08597759cc2 🚀

---
+++ _app/chunks/common-8fd32aff.js
@@ -0,0 +1,1 @@
+const e="Sylvain Corsini's Resume",t="Cloud & Fullstack Developer",o="online version",s="Print the resume",r="Dark mode",n="See more",l="See less";var i={title:e,job_title:t,qrcode_text:o,print_text:s,dark_mode:r,see_more:n,see_less:l};export{r as dark_mode,i as default,t as job_title,s as print_text,o as qrcode_text,l as see_less,n as see_more,e as title};
--- _app/pages/index.svelte-53c91c5d.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as e,s as n}from"../chunks/vendor-da950772.js";const r=async()=>({status:301,redirect:"/fr/"});class u extends t{constructor(s){super();e(this,s,null,null,n,{})}}export{u as default,r as load};
